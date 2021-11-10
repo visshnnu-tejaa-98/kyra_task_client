f93b4858c698c4a77cd616a0282bbc3348b6633b
visshnnu-xseed <85931077+visshnnu-xseed@users.noreply.github.com>
2021-11-10 15:11:17 +0530

    Add pagination

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 import { BACKEND_URL } from '../endpoints';
 
 const Posts = () => {
@@ -8,6 +9,7 @@ const Posts = () => {
 	const [postNames, setPostNames] = useState(null);
 	const [display, setDisplay] = useState(false);
 	const [search, setSearch] = useState('');
+
 	const wrapperRef = useRef(null);
 
 	const navigate = useNavigate();
@@ -74,6 +76,55 @@ const Posts = () => {
 		};
 	});
 
+	const [pageNumber, setPageNumber] = useState(0);
+	const usersPerPage = 10;
+	const pagesVisited = pageNumber * usersPerPage;
+	const displayPosts =
+		filterPosts &&
+		filterPosts.slice(pagesVisited, pagesVisited + usersPerPage).map((post) => {
+			return (
+				<div className='col col-sm-12 col-md-4 my-4 d-flex justify-content-center'>
+					<div class='card' style={{ width: '20rem' }}>
+						<div className='d-flex justify-content-between p-3'>
+							<span>
+								<span class='card-title mb-0 h5 pl-2'>{post.name}</span>
+							</span>
+							<div class=''>
+								<i
+									class='far fa-edit pt-1 px-1 pointer'
+									onClick={() => navigate(`/edit/${post._id}`)}
+								></i>
+								<i
+									class='far fa-trash-alt pt-1 px-1 pointer'
+									onClick={() => handleDelete(post)}
+								></i>
+							</div>
+						</div>
+						<img src={post.profileImg} class='card-img-top px-3 image-dimension' alt='...' />
+						<div class='card-body'>
+							<p class='card-text'>
+								<strong>Gender : </strong>
+								{post.gender}
+							</p>
+							<div class='card-description'>
+								<p class='card-text'>{post.description}</p>
+							</div>
+							<div>
+								{post.intrests &&
+									post.intrests.map((intrest) => (
+										<span class='badge badge-pill badge-warning mr-1'>{intrest}</span>
+									))}
+							</div>
+						</div>
+					</div>
+				</div>
+			);
+		});
+	const pageCount = filterPosts && Math.ceil(filterPosts.length / usersPerPage);
+
+	const changePage = ({ selected }) => {
+		setPageNumber(selected);
+	};
 	return (
 		<div className='posts container'>
 			<h1 className='d-flex justify-content-center mt-2'>All Posts</h1>
@@ -122,46 +173,22 @@ const Posts = () => {
 					)}
 				</div>
 			</div>
-			<div className='row'>
-				{filterPosts &&
-					filterPosts.map((post) => (
-						<div className='col col-sm-12 col-md-4 my-4 d-flex justify-content-center'>
-							<div class='card' style={{ width: '20rem' }}>
-								<div className='d-flex justify-content-between p-3'>
-									<span>
-										<span class='card-title mb-0 h5 pl-2'>{post.name}</span>
-									</span>
-									<div class=''>
-										<i
-											class='far fa-edit pt-1 px-1 pointer'
-											onClick={() => navigate(`/edit/${post._id}`)}
-										></i>
-										<i
-											class='far fa-trash-alt pt-1 px-1 pointer'
-											onClick={() => handleDelete(post)}
-										></i>
-									</div>
-								</div>
-								<img src={post.profileImg} class='card-img-top px-3 image-dimension' alt='...' />
-								<div class='card-body'>
-									<p class='card-text'>
-										<strong>Gender : </strong>
-										{post.gender}
-									</p>
-									<div class='card-description'>
-										<p class='card-text'>{post.description}</p>
-									</div>
-									<div>
-										{post.intrests &&
-											post.intrests.map((intrest) => (
-												<span class='badge badge-pill badge-warning mr-1'>{intrest}</span>
-											))}
-									</div>
-								</div>
-							</div>
-						</div>
-					))}
-			</div>
+			<div className='row'>{displayPosts}</div>
+			<ReactPaginate
+				previousLabel={'<'}
+				nextLabel={'>'}
+				breakLabel='...'
+				pageCount={pageCount}
+				marginPagesDisplayed={2}
+				pageRangeDisplayed={2}
+				onPageChange={changePage}
+				containerClassName='pagination paginationBttns d-flex justify-content-center mt-3'
+				breakClassName='break-me'
+				previousLinkClassName='text-dark'
+				nextLinkClassName='text-dark'
+				disabledClassName='text-secondary'
+				activeClassName='text-primary'
+			/>
 		</div>
 	);
 };
